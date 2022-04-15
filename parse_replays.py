83d912bf6d6bc45e937f5b095b323592bd282881
Stephen <st624@cornell.edu>
2022-04-14 20:41:23 -0400

    batched parser done

--- parse_replays.py
+++ parse_replays.py
@@ -4,18 +4,22 @@ from slippi.parse import ParseEvent
 import pandas as pd
 from os import listdir
 from os.path import isfile, join
+from os import listdir, chdir
 from tqdm import tqdm 
 import argparse
 from unicodedata import name
+import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-c','--count', type=int, required = False, default = -1,
-                    help='number of files to parse')
+parser.add_argument('-b','--batch_size', type=int, required = False, default = 32,
+                    help='number of files to parse per batch')
+parser.add_argument('-rb', '--resume_batch', type=int, required=False, default = 1,
+                    help='batch number to resume from')
 parser.add_argument('-r','--replays', type=str, default = 'Replays',
-                    help='replays folder')
+                    help='replays folder file path')
 parser.add_argument('-o','--output', type=str, default = 'Data',
-                    help='output folder')
-parser.add_argument('-n','--name', type=str, default = 'replay_by_frame',
+                    help='output folder file path')
+parser.add_argument('-n','--out_name', type=str, default = 'replay_by_frame',
                     help='output file name') 
 
 args = parser.parse_args()
@@ -25,7 +29,7 @@ def parse_frame_at_port(game, frame, port):
 
 def read_game(file_name):
     try:
-        return Game(f'../Replays/{file_name}')
+        return Game(f'{args.replays}/{file_name}')
     except:
         print(f'Failed to parse {file_name}')
 
@@ -120,30 +124,44 @@ y_cols_P2 =['Game_ID','Frame','B_damage_P2', 'B_direction_P2',
        'B_raw_analog_P2', 'B_buttons_physical_P2', 'B_triggers_physical_l_P2',
        'B_triggers_physical_r_P2', 'B_triggers_logical_P2']
 
-files = listdir('../Replays/')
-df = replays_to_df(files[:5])
 
-df = get_states(df)
-df = get_buttons(df)
 
-df_X = df[X_cols]
-df_y_P1 = df[y_cols_P1]
-df_y_P2 = df[y_cols_P2]
+files = listdir(f'{args.replays}/')
+chdir(f'{args.output}/')
 
-df_y_P1['Frame'] = df_y_P1['Frame'].apply(lambda x : x - 1)
-df_y_P2['Frame'] = df_y_P2['Frame'].apply(lambda x : x - 1)
 
-out1 = pd.merge(df_X, df_y_P1, on=['Game_ID', 'Frame'])
+batch_number = args.resume_batch
+for i in range((args.resume_batch - 1) * args.batch_size, len(files), args.batch_size):
+    print(i, i + args.batch_size)
+    
 
-df_X2 = df_column_switch(df_X, 'CHAR_P1', 'CHAR_P2')
+    #df.to_csv(f'{args.name}' + '.csv', index=False)
 
-df_y_P2 = df_y_P2.reindex(columns=['Game_ID','Frame','B_damage_P1',
-       'B_direction_P1', 'B_joystick_x_P1', 'B_joystick_y_P1',
-       'B_position_x_P1', 'B_position_y_P1', 'B_cstick_x_P1', 'B_cstick_y_P1',
-       'B_state_P1', 'B_raw_analog_P1', 'B_buttons_physical_P1',
-       'B_triggers_physical_l_P1', 'B_triggers_physical_r_P1',
-       'B_triggers_logical_P1'])
+    print(f'{batch_number}/{int(np.ceil(len(files) / args.batch_size))} Completed')
+    batch_number += 1
+# df = replays_to_df(files[:5])
 
-out2 = pd.merge(df_X2, df_y_P2, on=['Game_ID', 'Frame'])
+# df = get_states(df)
+# df = get_buttons(df)
 
-out = pd.concat([out1,out2],ignore_index=True)
+# df_X = df[X_cols]
+# df_y_P1 = df[y_cols_P1]
+# df_y_P2 = df[y_cols_P2]
+
+# df_y_P1['Frame'] = df_y_P1['Frame'].apply(lambda x : x - 1)
+# df_y_P2['Frame'] = df_y_P2['Frame'].apply(lambda x : x - 1)
+
+# out1 = pd.merge(df_X, df_y_P1, on=['Game_ID', 'Frame'])
+
+# df_X2 = df_column_switch(df_X, 'CHAR_P1', 'CHAR_P2')
+
+# df_y_P2 = df_y_P2.reindex(columns=['Game_ID','Frame','B_damage_P1',
+#        'B_direction_P1', 'B_joystick_x_P1', 'B_joystick_y_P1',
+#        'B_position_x_P1', 'B_position_y_P1', 'B_cstick_x_P1', 'B_cstick_y_P1',
+#        'B_state_P1', 'B_raw_analog_P1', 'B_buttons_physical_P1',
+#        'B_triggers_physical_l_P1', 'B_triggers_physical_r_P1',
+#        'B_triggers_logical_P1'])
+
+# out2 = pd.merge(df_X2, df_y_P2, on=['Game_ID', 'Frame'])
+
+# out = pd.concat([out1,out2],ignore_index=True)
