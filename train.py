1d422921cb4c3c81b5124d9303e20ade873c6e7c
Stephen <st624@cornell.edu>
2022-04-30 22:07:54 -0400

    added model checkpointing and cleaned train script

--- train.py
+++ train.py
@@ -17,22 +17,28 @@ from sklearn.feature_extraction.text import CountVectorizer
 from data_module import FrameDataset
 from ff_network import Net
 
+from utils import createdirs, log
+
 from tqdm import tqdm
+from datetime import datetime
+import argparse
 
-dataset = 'mini_state'
+parser = argparse.ArgumentParser(description='SCS Train')
+parser.add_argument('--model', default='', choices = ['rnn', 'ff'], help='model type')
+parser.add_argument('--dataset', default='', help='model type')
+args = parser.parse_args()
 
+#check for gpu
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+#get dataset
+dataset = args.dataset
+
+#read in data and create dataloaders
 x_train = pd.read_csv(f'Datasets/{dataset}_x_train.csv')
 y_train = pd.read_csv(f'Datasets/{dataset}_y_train.csv')
 x_test = pd.read_csv(f'Datasets/{dataset}_x_test.csv')
 y_test = pd.read_csv(f'Datasets/{dataset}_y_test.csv')
-
-X_train_scaler = StandardScaler().fit(x_train)
-x_train = X_train_scaler.transform(x_train)
-x_test = X_train_scaler.transform(x_test)
-
-y_train_scaler = StandardScaler().fit(y_train)
-y_train = y_train_scaler.transform(y_train)
-y_test = y_train_scaler.transform(y_test)
 
 train = FrameDataset(x_train,y_train)
 test = FrameDataset(x_test,y_test)
@@ -40,6 +46,7 @@ test = FrameDataset(x_test,y_test)
 training_loader = DataLoader(train)
 testing_loader = DataLoader(test)
 
+#model params
 batch_size = 2048
 max_lr = .05
 n_epochs = 100
@@ -48,109 +55,76 @@ input_size = x_train.shape[1]
 output_size = y_train.shape[1]
 h_size = 64
 
+network_gen = {
+    "ff": Net(input_size=input_size, output_size=output_size,hidden_size=h_size)
+}
+
+model_gen = network_gen.get(args.model)
+
 steps_per_epoch = len(training_loader)
 
-# Allow for a version to be provided at the command line, as in
-if len(sys.argv) > 1:
-    version = sys.argv[1]
-else:
-    version = "test"
+network = model_gen().to(device)
+print(f"Training: {args.model}")
 
-# Lay out the desitinations for all the results.
-accuracy_results_path = os.path.join(f"results", f"accuracy_{version}.npy")
-accuracy_history_path = os.path.join(
-    "results", f"accuracy_history_{version}.npy")
-loss_results_path = os.path.join("results", f"loss_{version}.npy")
-os.makedirs("results", exist_ok=True)
+optimizer = optim.Adam(network.parameters(), lr=max_lr)
 
-# Restore any previously generated results.
-try:
-    accuracy_results = np.load(accuracy_results_path).tolist()
-    accuracy_histories = np.load(accuracy_history_path).tolist()
-    loss_results = np.load(loss_results_path).tolist()
-except Exception:
-    loss_results = []
-    accuracy_results = []
-    accuracy_histories = []
+path = 'logs/' + args.model + '/' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+createdirs(path)
+log_file = open(path + '.log', 'w+')
 
-for i_run in range(n_runs):
-    network = Net(input_size=input_size, output_size=output_size,hidden_size=h_size)
-    # print(f"Model has {network.n_params()} parameters
-    optimizer = optim.Adam(network.parameters(), lr=max_lr)
-    scheduler = OneCycleLR(
-        optimizer,
-        max_lr=max_lr,
-        steps_per_epoch=steps_per_epoch,
-        epochs=n_epochs)
+scheduler = OneCycleLR(
+    optimizer,
+    max_lr=max_lr,
+    steps_per_epoch=steps_per_epoch,
+    epochs=n_epochs)
 
-    epoch_accuracy_history = []
-    for i_epoch in range(n_epochs):
+epoch_accuracy_history = []
+for i_epoch in range(n_epochs):
+    epoch_start_time = time.time()
+    epoch_training_loss = 0
+    epoch_testing_loss = 0
+    epoch_training_num_correct = 0
+    epoch_testing_num_correct = 0
 
-        epoch_start_time = time.time()
-        epoch_training_loss = 0
-        epoch_testing_loss = 0
-        epoch_training_num_correct = 0
-        epoch_testing_num_correct = 0
+    with tqdm(enumerate(training_loader)) as tqdm_training_loader:
+        for batch_idx, batch in tqdm_training_loader:
 
-        with tqdm(enumerate(training_loader)) as tqdm_training_loader:
-            for batch_idx, batch in tqdm_training_loader:
+            frames, labels = batch
+            preds = network(frames)
+            loss = F.mse_loss(preds, labels)
 
-                frames, labels = batch
-                preds = network(frames)
-                loss = F.mse_loss(preds, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
 
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-                scheduler.step()
+            epoch_training_loss += loss.item() * training_loader.batch_size
+            epoch_training_num_correct += (preds.argmax(dim=1).eq(labels).sum().item())
 
-                epoch_training_loss += loss.item() * training_loader.batch_size
-                epoch_training_num_correct += (preds.argmax(dim=1).eq(labels).sum().item())
+            tqdm_training_loader.set_description(
+                f'Step: {batch_idx + 1}/{steps_per_epoch}, '
+                f'Epoch: {i_epoch + 1}/{n_epochs}, '
+            )
 
-                tqdm_training_loader.set_description(
-                    f'Step: {batch_idx + 1}/{steps_per_epoch}, '
-                    f'Epoch: {i_epoch + 1}/{n_epochs}, '
-                    f'Run: {i_run + 1}/{n_runs}'
-                )
+    epoch_duration = time.time() - epoch_start_time
+    training_loss = epoch_training_loss / len(training_loader.dataset)
+    training_accuracy = (epoch_training_num_correct / len(training_loader.dataset))
 
-        epoch_duration = time.time() - epoch_start_time
-        training_loss = epoch_training_loss / len(training_loader.dataset)
-        training_accuracy = (epoch_training_num_correct / len(training_loader.dataset))
+    with torch.no_grad():
+        for batch in testing_loader:
+            images, labels = batch
+            preds = network(images)
+            loss = F.cross_entropy(preds, labels)
 
-        # At the end of each epoch run the testing data through an
-        # evaluation pass to see how the model is doing.
-        # Specify no_grad() to prevent a nasty out-of-memory condition.
-        with torch.no_grad():
-            for batch in testing_loader:
-                images, labels = batch
-                preds = network(images)
-                loss = F.cross_entropy(preds, labels)
+            epoch_testing_loss += loss.item() * testing_loader.batch_size
+            epoch_testing_num_correct += (preds.argmax(dim=1).eq(labels).sum().item())
 
-                epoch_testing_loss += loss.item() * testing_loader.batch_size
-                epoch_testing_num_correct += (preds.argmax(dim=1).eq(labels).sum().item())
+        testing_loss = epoch_testing_loss / len(testing_loader.dataset)
+        testing_accuracy = (epoch_testing_num_correct / len(testing_loader.dataset))
+        epoch_accuracy_history.append(testing_accuracy)
 
-            testing_loss = epoch_testing_loss / len(testing_loader.dataset)
-            testing_accuracy = (epoch_testing_num_correct / len(testing_loader.dataset))
-            epoch_accuracy_history.append(testing_accuracy)
+    log('Epoch {} Train Loss {} Train Accuracy {} Test Loss {} Test Accuracy {}'.format(
+    i_epoch, training_loss, training_accuracy, testing_loss, testing_accuracy), log_file)
 
-        print(
-            f"run: {i_run}   "
-            f"epoch: {i_epoch}   "
-            f"duration: {epoch_duration:.04}   "
-            f"learning rate: {scheduler.get_last_lr()[0]:.04}   "
-            f"training loss: {training_loss:.04}   "
-            f"testing loss: {testing_loss:.04}   "
-            f"training accuracy: {100 * training_accuracy:.04}%   "
-            f"testing accuracy: {100 * testing_accuracy:.04}%"
-        )
-
-        accuracy_histories.append(epoch_accuracy_history)
-        accuracy_results.append(testing_accuracy)
-        loss_results.append(testing_loss)
-
-        np.save(accuracy_history_path, np.array(accuracy_histories))
-        np.save(accuracy_results_path, np.array(accuracy_results))
-        np.save(loss_results_path, np.array(loss_results))
-
-
-
+torch.save(network.state_dict(), path + '.pt')
+log_file.close()
